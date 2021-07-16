dece628a546970c7b008207def2f8930db593b8b
Carmela Monis <carmelamonis@gmail.com>
2021-07-15 23:03:09 -0700

    Create map

--- Leaflet-Step-1/static/js/config.js
+++ Leaflet-Step-1/static/js/config.js
@@ -1,2 +1,2 @@
 // API key
-const API_KEY = "YOUR API KEY HERE!";
+const API_KEY = "pk.eyJ1IjoiY21vbmlzIiwiYSI6ImNrcXN0MXN0NzFzMWwybnFsMjc0YzZiZzgifQ.He9te7IrhtCcXgg36QSmNg";
---
+++ Leaflet-Step-1/static/js/logic.js
@@ -0,0 +1,76 @@
+var queryURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
+var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "light-v10",
+  accessToken: API_KEY
+});
+
+//Create map
+var myMap = L.map("map", {
+    center: [37.0902, -95.7129],
+    zoom: 5
+});
+
+lightmap.addTo(myMap); 
+
+//function createFeatures
+//for each features
+d3.json(queryURL).then(function(earthquakeData) {
+    
+    var features = earthquakeData.features;
+    var properties = earthquakeData.features.properties;
+    var geometry = earthquakeData.features.geometry;
+    //console.log(earthquakeData.features)
+    //var depth = geometry.coordinates[2];
+    var depth = 0;
+    
+    for (var i = 0; i < features.length; i++) {
+        //console.log(features[i].geometry.coordinates[2]);
+        var depth = +features[i].geometry.coordinates[2];
+        var lat = +features[i].geometry.coordinates[1];
+        var lon = +features[i].geometry.coordinates[0];
+        var mag = +features[i].properties.mag;
+
+        //Conditions for colors
+        var color = "";
+        if (depth >= -10 && depth < 10) {
+            color = "#28B463";
+        }
+        else if (depth >= 10 && depth < 30) { 
+            color = "#DAFF33";
+        }
+        else if (depth >= 30 && depth < 50) {
+            color = "#FAD7A0";
+        }
+        else if (depth >= 50 && depth < 70) {
+            color = "#F5B041";
+        }
+        else if (depth >= 70 && depth < 90) {
+            color = "#DC7633";
+        }
+        else {
+            color = "#C0392B";
+        };
+
+        //Info variables
+        var title = features[i].properties.title;
+        var place = features[i].properties.place;
+
+        //Add circles to map
+        L.circleMarker([lat, lon], {
+            fillOpacity: 1,
+            fillColor: color,
+            color: "black",
+            radius: mag * 4,
+            weight: .5
+        }).bindPopup("<h2>" + title + 
+        "</h2> <hr> <h3>Magnitude: " + mag + "</h3>" +
+        "<h3>Location: " + place + "</h3>" +
+        "<h3>Coordinates: [" + lat + " , " + lon + "]</h3>").addTo(myMap);
+
+    };
+ 
+});
+
